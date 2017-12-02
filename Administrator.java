7786a40df461868e59efabd0967e38647d53dcd6
AthensCabal <athenwerccaballero@yahoo.com>
2017-12-03 02:16:41 +0800

    Add files via upload

---
+++ Administrator.java
@@ -0,0 +1,35 @@
+public class Administrator extends Accounts {
+
+	private String EID;
+
+	private String password;
+
+	private String name;
+
+	private String department;
+
+	public operation0(String EID, String pass, String name, String department) {
+
+	}
+
+	public String getEID() {
+		return null;
+	}
+
+	public String getName() {
+		return null;
+	}
+
+	public String getDepartment() {
+		return null;
+	}
+
+	public String getPassword() {
+		return null;
+	}
+
+	public void modPassword() {
+
+	}
+
+}
