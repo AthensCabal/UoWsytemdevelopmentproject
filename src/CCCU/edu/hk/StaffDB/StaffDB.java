3d58c9cf98277e7cbaa1664cafd1edd5df3824aa
manishmall20 <manish147220@gmail.com>
2017-12-06 20:41:04 +0800

    Minor Fixes

--- src/CCCU/edu/hk/StaffDB/StaffDB.java
+++ src/CCCU/edu/hk/StaffDB/StaffDB.java
@@ -61,10 +61,24 @@ public class StaffDB implements StaffDB_Interface {
 
             //##INITIAL SQL SECTION##
             //Create a statement to issue simple commands.
+            s = conn.createStatement();
+            
+            //this truly deletes just incase the cleanup() fails to delete the first database            
+            DatabaseMetaData dbmd = conn.getMetaData();
+            ResultSet rs = dbmd.getTables(null, null, dbName.toUpperCase(), null);
+
+            if (rs.next()) {
+
+                String sql = "DROP TABLE " + dbName.toUpperCase();
+                s.executeUpdate(sql);
+
+            }
+            
+            //##INITIAL SQL SECTION##
+            //Create a statement to issue simple commands.
             ps = conn.prepareStatement(createTable);
 
-            //this truly deletes just incase the cleanup() fails to delete the first database
-            //DatabaseMetaData dbmd = conn.getMetaData();
+            ;
 
             //This creates a table
             ps.executeUpdate();
@@ -121,6 +135,8 @@ public class StaffDB implements StaffDB_Interface {
         } catch (Throwable e) {
 
             //When the server is off,prompt exit program
+            
+            /*
             System.out.println("StaffDatabase could not be connected. Connect to the server and try again.");
             System.out.println("Do you still wish to continue the program? Y/N ");
             Scanner in = new Scanner(System.in);
@@ -135,8 +151,12 @@ public class StaffDB implements StaffDB_Interface {
                 default:
                     System.out.println("The program will continue to run but do note that none of the choice will work.");
                     break;
-
+            
             }
+*/
+            e.printStackTrace(System.out);
+            
+            
         }
 
     }
